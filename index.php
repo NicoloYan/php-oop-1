6776c6d61f8ec0fb5c178e542db2e9a14b8e4064
NicoloYan <nico.yan@outlook.it>
2022-07-27 14:35:48 +0200

    Printed objects and their properties

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php 
+class Movie {
+
+    public $title;
+
+    public $releaseDate;
+
+    public $length = 0;
+
+}
+
+
+// FILM 1
+$LOTR = new Movie();
+
+$LOTR->title = 'The Lord of the Rings: The Return of the King';
+$LOTR->releaseDate = '22/01/2004';
+$LOTR->length = 201;
+
+// FILM 2
+$SW = new Movie();
+
+$SW->title = 'Star Wars: Episode VI: Return of the Jedi';
+$SW->releaseDate = '21/10/1983';
+$SW->length = 134;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>
+            Film 1
+        </h1>
+        <h3>
+            Titolo: <?php echo $LOTR->title; ?>
+        </h3>
+        <h3>
+            Data di uscita: <?php echo $LOTR->releaseDate; ?>
+        </h3>
+        <h3>
+            Durata: <?php echo $LOTR->length; ?> minuti
+        </h3>
+    </div>
+    <div>
+        <h1>
+            Film 2
+        </h1>
+        <h3>
+            Titolo: <?php echo $SW->title; ?>
+        </h3>
+        <h3>
+            Data di uscita: <?php echo $SW->releaseDate; ?>
+        </h3>
+        <h3>
+            Durata: <?php echo $SW->length; ?> minuti
+        </h3>
+    </div>
+</body>
+</html>
